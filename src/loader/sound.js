feffbab987b38de7a3ccd4cf212e31f69fac1a04
Маков Андрей Вячеславович <av_makov@bingo-boom.com>
2020-03-13 13:29:39 +0300

    fix sound loader

--- src/loader/sound.js
+++ src/loader/sound.js
@@ -1,4 +1,4 @@
 import { Howl } from "howler";
 
 export default ( {path}, { revision, rel, ...args } ) =>
-  Promise.resolve({url: `${path}${rel}`, type: "sound", sound: new Howl({src: []}), ...args});
+  Promise.resolve({url: `${path}${rel}`, type: "sound", sound: {play(){}}, ...args});
